192fef944b84eaa388ece045e6ba434b34cf5fdc
loiane <loianeg@gmail.com>
2016-09-15 09:15:09 -0300

    added pokemon-species component - first commit

---
+++ src/app/pokemon-details/pokemon-species/pokemon-species.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PokemonSpeciesComponent } from './pokemon-species.component';
+
+describe('Component: PokemonSpecies', () => {
+  it('should create an instance', () => {
+    let component = new PokemonSpeciesComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pokemon-details/pokemon-species/pokemon-species.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-species',
+  templateUrl: './pokemon-species.component.html',
+  styleUrls: ['./pokemon-species.component.scss']
+})
+export class PokemonSpeciesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
