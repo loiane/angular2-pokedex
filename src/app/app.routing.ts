48a8033ec8e0648ba3dcb24236c19623fea5370e
loiane <loianeg@gmail.com>
2016-09-15 09:08:34 -0300

    added app routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule }   from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/pokedex',
+    pathMatch: 'full'
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
