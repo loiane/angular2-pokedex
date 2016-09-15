2cca1727dc5e85cd9388d366b2320c904b6381bd
loiane <loianeg@gmail.com>
2016-09-15 09:06:03 -0300

    added pokedex component - main screen

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PokedexService } from './pokedex.service';
+
 @Component({
   selector: 'app-pokedex',
   templateUrl: 'pokedex.component.html',
-  styleUrls: ['pokedex.component.css']
+  styleUrls: ['pokedex.component.scss']
 })
 export class PokedexComponent implements OnInit {
 
-  constructor() { }
+  pokemonList: any[] = [];
 
-  ngOnInit() {
+  private searchQuery: string = '';
+
+  constructor(private pokedexService: PokedexService) { }
+
+  ngOnInit(){
+    this.pokedexService.getAllPokemon()
+      .subscribe(pokemonList => this.pokemonList = pokemonList);
+  }
+
+  getPokemons(){
+    var q = this.searchQuery;
+    if (q.trim() == '') {
+      return this.pokemonList;
+    }
+    return this.pokemonList.filter((v) => {
+      if (v.name.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
+        return true;
+      }
+      return false;
+    });
   }
 
 }
