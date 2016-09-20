ba0a055b02f93f00823feecc525e17d52c2b5c2f
loiane <loianeg@gmail.com>
2016-09-20 09:56:43 -0300

    added pokemon species

--- src/app/pokemon-details/pokemon-species/pokemon-species.component.ts
+++ src/app/pokemon-details/pokemon-species/pokemon-species.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { PokedexService } from '../../pokedex/pokedex.service';
+import { UtilitiesService } from '../../shared/utilities.service';
 
 @Component({
   selector: 'app-pokemon-species',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonSpeciesComponent implements OnInit {
 
-  constructor() { }
+  @Input() pokemon: any;
+
+  private speciesDetails: any;
+  private speciesName: string;
+  private description: string;
+  private abilities: any;
+
+  constructor(
+    private pokedexService: PokedexService,
+    private util: UtilitiesService
+  ) {}
 
   ngOnInit() {
+    let speciesId: number = this.util.retrieveIdFromUrl(this.pokemon.species.url, 'pokemon-species');
+    this.pokedexService.getSpecies(speciesId)
+      .subscribe(data => this.initSpeciesData(data));
+  }
+
+  initSpeciesData(data){
+    this.speciesDetails = data;
+    this.speciesName = this.util.getTranslatedName(this.speciesDetails.genera);
+    this.description = this.util.getTranslatedName(this.speciesDetails.flavor_text_entries);
+    this.abilities = this.getPokemonAbilities();
+  }
+
+  getPokemonAbilities(){
+    let list: any[] = [];
+    let infoList = this.pokemon.abilities;
+    if (this.pokemon && infoList) {
+      let item: any;
+      for (let i=0; i<infoList.length; i++){
+        item = infoList[i];
+        if (item){
+          list.splice((item.slot-1), 0, item);
+        }
+      }
+    }
+    return list;
   }
 
 }
