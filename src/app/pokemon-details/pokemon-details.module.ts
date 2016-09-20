8507bdbd211ffd0a1d1a56bc3c2e1ea4c4c75d91
loiane <loianeg@gmail.com>
2016-09-20 10:00:16 -0300

    added pokemon-details sections

--- src/app/pokemon-details/pokemon-details.module.ts
+++ src/app/pokemon-details/pokemon-details.module.ts
@@ -9,6 +9,15 @@ import { PokemonDetailsComponent } from './pokemon-details.component';
 import { PokemonImageComponent } from './pokemon-image/pokemon-image.component';
 import { PokemonHeaderComponent } from './pokemon-header/pokemon-header.component';
 import { PokemonSpeciesComponent } from './pokemon-species/pokemon-species.component';
+import { PokemonStatsComponent } from './pokemon-stats/pokemon-stats.component';
+import { PokemonStatsItemComponent } from './pokemon-stats-item/pokemon-stats-item.component';
+import { PokemonEvolutionComponent } from './pokemon-evolution/pokemon-evolution.component';
+import { PokemonSpritesComponent } from './pokemon-sprites/pokemon-sprites.component';
+import { PokemonTypeEffectivenessComponent } from './pokemon-type-effectiveness/pokemon-type-effectiveness.component';
+import { PokemonTypeEffectivenessItemComponent } from './pokemon-type-effectiveness-item/pokemon-type-effectiveness-item.component';
+import { PokemonMovesComponent } from './pokemon-moves/pokemon-moves.component';
+import { PokemonMovesDetailComponent } from './pokemon-moves-detail/pokemon-moves-detail.component';
+import { PokemonMovesDetailItemComponent } from './pokemon-moves-detail-item/pokemon-moves-detail-item.component';
 
 @NgModule({
   imports: [
@@ -21,7 +30,16 @@ import { PokemonSpeciesComponent } from './pokemon-species/pokemon-species.compo
     PokemonDetailsComponent,
     PokemonImageComponent,
     PokemonHeaderComponent,
-    PokemonSpeciesComponent
+    PokemonSpeciesComponent,
+    PokemonStatsComponent,
+    PokemonStatsItemComponent,
+    PokemonEvolutionComponent,
+    PokemonSpritesComponent,
+    PokemonTypeEffectivenessComponent,
+    PokemonTypeEffectivenessItemComponent,
+    PokemonMovesComponent,
+    PokemonMovesDetailComponent,
+    PokemonMovesDetailItemComponent
   ],
   exports: [
     PokemonDetailsComponent,
